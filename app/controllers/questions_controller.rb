13d9bf7456583cd830fe4d4d32fba1469818a925
Timothy Hollis <timothyhollis@Timothys-MacBook-Pro.local>
2018-04-18 14:27:09 -0400

    added ability to save questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,23 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to '/questions'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
 end
